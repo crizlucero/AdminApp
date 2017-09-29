e57533910754daef4e65ba3f705c019c541fa2ed
Christian Lucero <crizlucero@gmail.com>
2017-09-29 17:06:14 -0500

    Android Project
     - Fix bug with the null url images.

--- WorklabsMx.Droid/Helpers/ImagesHelper.cs
+++ WorklabsMx.Droid/Helpers/ImagesHelper.cs
@@ -1,37 +1,38 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Net;
 using Android.Graphics;
 using Android.Graphics.Drawables;
 using Android.Media;
 using Android.Net;
+using WorklabsMx.Helpers;
 
 namespace WorklabsMx.Droid.Helpers
 {
     public class ImagesHelper
     {
-        public static Uri GetPerfilImagen(string imagen)
-        {
-            return Uri.Parse("http://desarrolloworklabs.com/Dashboard_Client/usr_imgs/" + imagen);
-        }
-
-
+        public static Android.Net.Uri GetPerfilImagen(string imagen) =>
+             Android.Net.Uri.Parse("http://desarrolloworklabs.com/Dashboard_Client/usr_imgs/" + imagen);
 
         public static Bitmap GetImageBitmapFromUrl(string url)
         {
-            Bitmap imageBitmap = null;
-            byte[] bytes = null;
-
-            using (var webClient = new WebClient())
+            if (!string.IsNullOrEmpty(url))
             {
-                var imageBytes = webClient.DownloadData("http://desarrolloworklabs.com/Dashboard_Client/usr_imgs/" + url);
-                if (imageBytes != null && imageBytes.Length > 0)
-                {
-                    bytes = imageBytes;
-                    imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
-                }
+                Bitmap imageBitmap = null;
+                byte[] bytes = null;
 
+                using (var webClient = new WebClient())
+                {
+                    var imageBytes = webClient.DownloadData("http://desarrolloworklabs.com/Dashboard_Client/usr_imgs/" + url);
+                    if (imageBytes != null && imageBytes.Length > 0)
+                    {
+                        bytes = imageBytes;
+                        imageBitmap = BitmapFactory.DecodeByteArray(imageBytes, 0, imageBytes.Length);
+                    }
+                }
+                return imageBitmap;
             }
-            return imageBitmap;
+            return null;
         }
     }
 }
