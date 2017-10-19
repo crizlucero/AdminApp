a6ada8507007577bd3b52855bb85894d748294ba
gilberto117 <gilberto.e.barrera@gmail.com>
2017-10-19 14:17:12 -0500

    nueva version

--- iOS/ComentarPostHeaderCell.designer.cs
+++ iOS/ComentarPostHeaderCell.designer.cs
@@ -16,7 +16,19 @@ namespace WorklabsMx.iOS
     {
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnBorrarFoto { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnFotografia { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UIButton btnPublicar { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnSeleccionarImagen { get; set; }
 
         [Outlet]
         [GeneratedCode ("iOS Designer", "1.0")]
@@ -26,15 +38,42 @@ namespace WorklabsMx.iOS
         [GeneratedCode ("iOS Designer", "1.0")]
         UIKit.UIView vwComentarPost { get; set; }
 
+        [Action ("btnBorrarFoto_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void btnBorrarFoto_TouchUpInside (UIKit.UIButton sender);
+
         [Action ("btnComentar_TouchUpInside:")]
         [GeneratedCode ("iOS Designer", "1.0")]
         partial void btnComentar_TouchUpInside (UIKit.UIButton sender);
 
+        [Action ("btnFotografia_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void btnFotografia_TouchUpInside (UIKit.UIButton sender);
+
+        [Action ("btnSleccionarImagen_TouchUpInside:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void btnSleccionarImagen_TouchUpInside (UIKit.UIButton sender);
+
         void ReleaseDesignerOutlets ()
         {
+            if (btnBorrarFoto != null) {
+                btnBorrarFoto.Dispose ();
+                btnBorrarFoto = null;
+            }
+
+            if (btnFotografia != null) {
+                btnFotografia.Dispose ();
+                btnFotografia = null;
+            }
+
             if (btnPublicar != null) {
                 btnPublicar.Dispose ();
                 btnPublicar = null;
+            }
+
+            if (btnSeleccionarImagen != null) {
+                btnSeleccionarImagen.Dispose ();
+                btnSeleccionarImagen = null;
             }
 
             if (txtComentarPost != null) {
