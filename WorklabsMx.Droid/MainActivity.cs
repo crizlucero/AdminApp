8272c3b3445ba67f090790be689524512cb4c311
Christian Lucero <crizlucero@gmail.com>
2017-10-19 17:42:10 -0500

    Return to the dashboard in the menu

--- WorklabsMx.Droid/MainActivity.cs
+++ WorklabsMx.Droid/MainActivity.cs
@@ -462,7 +462,7 @@ namespace WorklabsMx.Droid
                 {
                     switch (menu.Controller)
                     {
-                        case "MainActivity": StartActivity(new Intent(this, typeof(MainActivity))); break;
+                        case "MainActivity": FindViewById<ScrollView>(Resource.Id.menu_scroll).Visibility = ViewStates.Gone; break;//StartActivity(new Intent(this, typeof(MainActivity))); break;
                         case "MyMembershipActivity": StartActivity(new Intent(this, typeof(MyMembershipActivity))); break;
                         case "SubMenuActivity":
                             localStorage.Put("Parent", menu.Menu_Id);
