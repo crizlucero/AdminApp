35b73fe59ecffde337a1da41ecc8d15f0b4fb1da
gilberto117 <gilberto.e.barrera@gmail.com>
2017-11-06 08:51:32 -0600

    test

---
+++ iOS/ColeccionProductos.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace WorklabsMx.iOS
+{
+    public partial class ColeccionProductos : UICollectionViewController
+    {
+        public ColeccionProductos (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
