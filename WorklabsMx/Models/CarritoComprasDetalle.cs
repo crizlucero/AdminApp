ffec4570260d850fabb495f382cb18c4b053e30b
Christian Lucero <crizlucero@gmail.com>
2017-11-09 10:34:25 -0600

    Added the shopping cart changes

---
+++ WorklabsMx/Models/CarritoComprasDetalle.cs
@@ -0,0 +1,31 @@
+﻿namespace WorklabsMx.Models
+{
+    public class CarritoComprasDetalle
+    {
+        public string Membresia_Id { get; set; }
+        public string Inscripcion_Membresia_Id { get; set; }
+        public string Lista_Precio_Membresia_Id { get; set; }
+        public string Producto_Id { get; set; }
+        public string Lista_Precio_Producto_Id { get; set; }
+        public string Servicio_Id { get; set; }
+        public string Lista_Precio_Servicio_Id { get; set; }
+        public string Carrito_Compras_Detalle_Descripcion { get; set; }
+        public string Carrito_Compras_Detalle_Cantidad { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Precio { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Prorrateo { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Suma { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Descuento { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Subtotal { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Impuesto { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Total { get; set; }
+        public string Carrito_Compras_Detalle_Estatus { get; set; }
+        public string Carrito_Compras_Detalle_Vigenci_Fecha_Inicio { get; set; }
+        public string Carrito_Compras_Detalle_Vigenci_Fecha_Fin { get; set; }
+        public string Carrito_Compras_Detalle_Vigenci_Fecha { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Suma_Texto { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Descuento_Texto { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Subtotal_Texto { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Impuesto_Texto { get; set; }
+        public string Carrito_Compras_Detalle_Importe_Total_Texto { get; set; }
+    }
+}
