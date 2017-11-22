63429fd20cce5670ffa4358f97f80642c1de8e0d
gilberto117 <gilberto.e.barrera@gmail.com>
2017-11-22 10:19:45 -0600

    version para subir a la tienda

---
+++ iOS/cloudViewController.cs
@@ -0,0 +1,59 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace WorklabsMx.iOS
+{
+    public partial class cloudViewController : UITableViewController
+    {
+        
+        const string IdentificadorCeldaInfo = "Info";
+        const string IdentificadorCeldaNoInfo = "NoInfo";
+
+
+        const int TamañoCeldaInfo = 70;
+        const int TamañoMensajeNoInfo = 400;
+
+        bool isShowInformation = false;
+        bool existeConeccion = true;
+
+
+        public cloudViewController (IntPtr handle) : base (handle)
+        {
+            
+        }
+
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+
+        public override void ViewWillAppear(bool animated)
+        {
+            base.ViewWillAppear(animated);
+        }
+
+        public override void ViewDidAppear(bool animated)
+        {
+            base.ViewDidAppear(animated);
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            return 1;
+        }
+
+        public override nfloat GetHeightForRow(UITableView tableView, Foundation.NSIndexPath indexPath)
+        {
+            return TamañoMensajeNoInfo;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, Foundation.NSIndexPath indexPath)
+        {
+            var noPostCell = tableView.DequeueReusableCell(IdentificadorCeldaNoInfo, indexPath);
+            return noPostCell;
+        }
+
+    }
+}
