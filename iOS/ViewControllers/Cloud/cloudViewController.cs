fd183f90f4b66f76f496099bad1debc822a5a161
gilberto117 <gilberto.e.barrera@gmail.com>
2017-11-27 17:21:09 -0600

    nueva vista de editar perfil y datos de empresa

--- iOS/ViewControllers/Cloud/cloudViewController.cs
+++ iOS/ViewControllers/Cloud/cloudViewController.cs
@@ -2,10 +2,6 @@ using System;
 using UIKit;
 using System.Collections.Generic;
 using WorklabsMx.Models;
-using BigTed;
-using WorklabsMx.Controllers;
-using WorklabsMx.iOS.Helpers;
-using WorklabsMx.iOS.Models;
 
 namespace WorklabsMx.iOS
 {
