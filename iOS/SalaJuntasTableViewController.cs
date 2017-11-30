d45481f7e7f59984f3dd792dcc0e005dbefac07e
gilberto117 <gilberto.e.barrera@gmail.com>
2017-11-30 09:53:31 -0600

    version con sala de juntas 1.0.2

---
+++ iOS/SalaJuntasTableViewController.cs
@@ -0,0 +1,19 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace WorklabsMx.iOS
+{
+    public partial class SalaJuntasTableViewController : UITableViewController
+    {
+        public SalaJuntasTableViewController (IntPtr handle) : base (handle)
+        {
+            
+        }
+
+        partial void btnEditar_TouchUpInside(UIButton sender)
+        {
+            
+        }
+    }
+}
