bed2348e55d1bf1be743d0bb3932c97a2780bdbd
gilberto117 <gilberto.e.barrera@gmail.com>
2017-12-13 18:48:40 -0600

    reservacionn sala juntas en proceso

--- WorklabsMx/Controllers/SucursalController.cs
+++ WorklabsMx/Controllers/SucursalController.cs
@@ -47,7 +47,7 @@ namespace WorklabsMx.Controllers
         public List<SucursalModel> GetSucursales()
         {
             List<SucursalModel> sucursales = new List<SucursalModel>();
-            string query = "SELECT Sucursal_Domicilio, Sucursal_Descripcion, Sucursal_Imagen FROM vw_cat_Sucursales";
+            string query = "SELECT Sucursal_Domicilio, Sucursal_Descripcion FROM vw_cat_Sucursales";
             try
             {
                 conn.Open();
@@ -58,8 +58,8 @@ namespace WorklabsMx.Controllers
                     sucursales.Add(new SucursalModel
                     {
                         Sucursal_Domicilio = reader["Sucursal_Domicilio"].ToString(),
-                        Sucursal_Descripcion = reader["Sucursal_Descripcion"].ToString(),    
-                        Sucursal_Imagen = reader["Sucursal_Imagen"].ToString()                                               
+                        Sucursal_Descripcion = reader["Sucursal_Descripcion"].ToString()    
+                        //Sucursal_Imagen = reader["Sucursal_Imagen"].ToString()                                               
                     });
 
                 }
