9e5e1d27399c9486a996d2e2b7d7170c2f03537a
gilberto117 <gilberto.e.barrera@gmail.com>
2018-01-16 16:40:07 -0600

    inicio diseño invitados

---
+++ iOS/RegistroInvitadosTableViewController.cs
@@ -0,0 +1,60 @@
+using Foundation;
+using System;
+using UIKit;
+using SWRevealViewControllerBinding;
+
+
+namespace WorklabsMx.iOS
+{
+    public partial class RegistroInvitadosTableViewController : UITableViewController
+    {
+        int NumeroCeldasInvitados = 1;
+        public RegistroInvitadosTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+        }
+
+        partial void btnAñadir_Touch(UIButton sender)
+        {
+            TableView.BeginUpdates();
+            TableView.InsertRows(new NSIndexPath[] { NSIndexPath.FromRowSection(NumeroCeldasInvitados, 0) }, UITableViewRowAnimation.Fade);
+            NumeroCeldasInvitados++;
+            TableView.EndUpdates();
+        }
+
+
+        partial void btnFecha_Tocuh(UIButton sender)
+        {
+        }
+
+        partial void btnUbicacion_Touch(UIButton sender)
+        {
+        }
+
+        partial void btnEnviarInvitacion_Touch(UIButton sender)
+        {
+        }
+
+        public override nint RowsInSection(UITableView tableView, nint section)
+        {
+            if (section == 0)
+            {
+                return NumeroCeldasInvitados;
+            }
+            else
+            {
+                return base.RowsInSection(TableView, section);
+            }
+        }
+
+        partial void btnMenuInvitados_Touch(UIBarButtonItem sender)
+        {
+            this.RevealViewController().RevealToggleAnimated(true);
+            View.AddGestureRecognizer(this.RevealViewController().PanGestureRecognizer);
+        }
+    }
+}
---
+++ iOS/RegistroInvitadosTableViewController.designer.cs
@@ -0,0 +1,130 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace WorklabsMx.iOS
+{
+    [Register ("RegistroInvitadosTableViewController")]
+    partial class RegistroInvitadosTableViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnAñadir { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnEnviarInvitacion { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnFecha { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIBarButtonItem btnMenuIvitados { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btnUbicacion { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblFecha { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel lblUbicacion { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtApellido { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtEmail { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField txtNombre { get; set; }
+
+        [Action ("btnAñadir_Touch:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void btnAñadir_Touch (UIKit.UIButton sender);
+
+        [Action ("btnEnviarInvitacion_Touch:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void btnEnviarInvitacion_Touch (UIKit.UIButton sender);
+
+        [Action ("btnFecha_Tocuh:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void btnFecha_Tocuh (UIKit.UIButton sender);
+
+        [Action ("btnMenuInvitados_Touch:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void btnMenuInvitados_Touch (UIKit.UIBarButtonItem sender);
+
+        [Action ("btnUbicacion_Touch:")]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        partial void btnUbicacion_Touch (UIKit.UIButton sender);
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btnAñadir != null) {
+                btnAñadir.Dispose ();
+                btnAñadir = null;
+            }
+
+            if (btnEnviarInvitacion != null) {
+                btnEnviarInvitacion.Dispose ();
+                btnEnviarInvitacion = null;
+            }
+
+            if (btnFecha != null) {
+                btnFecha.Dispose ();
+                btnFecha = null;
+            }
+
+            if (btnMenuIvitados != null) {
+                btnMenuIvitados.Dispose ();
+                btnMenuIvitados = null;
+            }
+
+            if (btnUbicacion != null) {
+                btnUbicacion.Dispose ();
+                btnUbicacion = null;
+            }
+
+            if (lblFecha != null) {
+                lblFecha.Dispose ();
+                lblFecha = null;
+            }
+
+            if (lblUbicacion != null) {
+                lblUbicacion.Dispose ();
+                lblUbicacion = null;
+            }
+
+            if (txtApellido != null) {
+                txtApellido.Dispose ();
+                txtApellido = null;
+            }
+
+            if (txtEmail != null) {
+                txtEmail.Dispose ();
+                txtEmail = null;
+            }
+
+            if (txtNombre != null) {
+                txtNombre.Dispose ();
+                txtNombre = null;
+            }
+        }
+    }
+}
