c28d8b139d3b65929a99b366230b751d975bad7e
Christian Lucero <crizlucero@gmail.com>
2018-02-09 16:59:48 -0600

    Mejoras

--- WorklabsMx/Models/DataBaseModel.cs
+++ WorklabsMx/Models/DataBaseModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.Data.SqlClient;
-using System.Net;
 
 namespace WorklabsMx.Models
 {
@@ -13,7 +12,7 @@ namespace WorklabsMx.Models
         protected SqlCommand command;
         protected SqlTransaction transaction;
 
-        protected DataBaseModel() => conn = new SqlConnection(@"Server=mssql.worklabs.mx\WLSQLSERVER; Database=WorklabsTest; User=developer; Pwd=d3p3l0p3r!");
+        protected DataBaseModel() => conn = new SqlConnection(@"Server=worklabs.mx\WLSQLSERVER; Database=WorklabsTest; User=developer; Pwd=d3p3l0p3r!");
 
         protected SqlCommand CreateCommand(string query) => new SqlCommand(query, conn);
 
