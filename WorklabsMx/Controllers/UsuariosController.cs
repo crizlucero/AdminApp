2fce6f8943b7f7518845760d684a8171da5f7494
Christian Lucero <crizlucero@gmail.com>
2018-02-23 12:33:02 -0600

    Added email in company

--- WorklabsMx/Controllers/UsuariosController.cs
+++ WorklabsMx/Controllers/UsuariosController.cs
@@ -64,6 +64,7 @@ namespace WorklabsMx.Controllers
                             Empresa_Id = reader["Usuario_Empresa_Id"].ToString(),
                             Empresa_Nombre = reader["Usuario_Empresa_Nombre"].ToString(),
                             Empresa_Logotipo = reader["Usuario_Empresa_Logotipo"].ToString(),
+                            Empresa_Correo_Electronico = reader["Empresa_Miembro_Correo_Electronico"].ToString(),
                             Empresa_Logotipo_Perfil = ImageHelper.DownloadFileFTP(reader["Usuario_Empresa_Logotipo"].ToString(), empresa_imagen_path),
                             Territorio = new TerritorioModel
                             {
