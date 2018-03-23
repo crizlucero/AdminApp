53a36f0da7133ebb01c8279dafd3091b593f687f
gilberto117 <gilberto.e.barrera@gmail.com>
2018-03-23 12:38:09 -0600

    guardar info de pagos en BD local

--- iOS/EjemploPagos.cs
+++ iOS/EjemploPagos.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using UIKit;
 using System.Collections.Generic;
-using BigTed;
 using WorklabsMx.iOS.Models;
 using WorklabsMx.Controllers;
 using WorklabsMx.Models;
-using Foundation;
-using System.Threading.Tasks;
-//using PerpetualEngine.Storage;
-using WorklabsMx.iOS.Helpers;
 using SuitemCommerce;
+using WorklabsMx.Handlers;
 
 namespace WorklabsMx.iOS
 {
@@ -19,18 +14,14 @@ namespace WorklabsMx.iOS
         Bean3DS bean3DS;
         BeanTokenization beanTokenization;
 
-        OrdenVentaController ordenventaLocal;
-        List<CarritoComprasDetalle> membresiasLocal;
 
         public EjemploPagos()
         {
         }
 
-        public void Pagar(VentaDetalleTableViewController VistaPagos, OrdenVentaController ordenventa, List<CarritoComprasDetalle> membresias)
+        public void Pagar(VentaDetalleTableViewController VistaPagos, string Total)
         {
-            this.ordenventaLocal = ordenventa;
-            this.membresiasLocal = membresias;
-            this.MostrarVistaTarjeta(VistaPagos, ordenventa.total);
+            this.MostrarVistaTarjeta(VistaPagos, Total);
         }
 
         private void MostrarVistaTarjeta(VentaDetalleTableViewController VistaPagos, string total)
@@ -67,22 +58,23 @@ namespace WorklabsMx.iOS
             suiteController.SndPayWithTokenWithBeanTokenization(beanTokenization, bean3DS);
         }
 
-        private void GuardarPagoServer(OrdenVentaController ordenventa, List<CarritoComprasDetalle> membresias)
+        private void GuardarPagoServer()
         {
+            PagosHandler pagos = new PagosHandler();
+            OrdenVentaEncabezado encabezado = pagos.GetEncabezado();
+            List<OrdenVentaDetalle> detalles = pagos.GetDetalles();
+
+
             bool VentaRealizada = false;
-            if (ordenventa.promocion_id == null)
-            {
-                ordenventa.promocion_id = "0";
-            }
 
             if (Helpers.InternetConectionHelper.VerificarConexion())
             {
-                int ValorEncabezado = new PagosController().AddOrdenVentaEncabezado(int.Parse(ordenventa.empresa_miembro_id), int.Parse(ordenventa.moneda_id), int.Parse(ordenventa.impuesto_id), int.Parse(ordenventa.promocion_id), int.Parse(ordenventa.descuento_id), ordenventa.folio, Convert.ToDecimal(ordenventa.suma), Convert.ToDecimal(ordenventa.porcentajeDecuento), Convert.ToDecimal(ordenventa.descuento), Convert.ToDecimal(ordenventa.subTotal), Convert.ToDecimal(ordenventa.impuesto), Convert.ToDecimal(ordenventa.total), Convert.ToDecimal(ordenventa.pagado), Convert.ToDecimal(ordenventa.facturado));
+                int ValorEncabezado = new PagosController().AddOrdenVentaEncabezado(encabezado.Usuario_Id, encabezado.Moneda_Id, encabezado.Impuesto_Id, encabezado.Promocion_Id, encabezado.Descuento_Id, encabezado.Folio, Convert.ToDecimal(encabezado.Importe_Suma), Convert.ToDecimal(encabezado.Porcentaje_Descuento), Convert.ToDecimal(encabezado.Importe_Descuento), Convert.ToDecimal(encabezado.Importe_Subtotal), Convert.ToDecimal(encabezado.Importe_Impuesto), Convert.ToDecimal(encabezado.Importe_Total), Convert.ToDecimal(encabezado.Importe_Pagado), Convert.ToDecimal(encabezado.Importe_Facturado));
                 if (ValorEncabezado >= 0)
                 {
-                    foreach (CarritoComprasDetalle membresia in membresias)
+                    foreach (OrdenVentaDetalle membresia in detalles)
                     {
-                        int ValorDetalle = new PagosController().AddOrdenVentaDetalle(ValorEncabezado, membresia.Membresia_Id, membresia.Inscripcion_Membresia_Id, membresia.Lista_Precio_Membresia_Id, membresia.Producto_Id, membresia.Lista_Precio_Producto_Id, membresia.Carrito_Compras_Detalle_Descripcion, int.Parse(membresia.Carrito_Compras_Detalle_Cantidad), Convert.ToDecimal(membresia.Carrito_Compras_Detalle_Importe_Precio), Convert.ToDecimal(membresia.Carrito_Compras_Detalle_Importe_Prorrateo), Convert.ToDecimal(membresia.Carrito_Compras_Detalle_Importe_Suma), Convert.ToDecimal(membresia.Carrito_Compras_Detalle_Importe_Descuento), Convert.ToDecimal(membresia.Carrito_Compras_Detalle_Importe_Subtotal), Convert.ToDecimal(membresia.Carrito_Compras_Detalle_Importe_Impuesto), Convert.ToDecimal(membresia.Carrito_Compras_Detalle_Importe_Total), membresia.Tipo);
+                        int ValorDetalle = new PagosController().AddOrdenVentaDetalle(ValorEncabezado, membresia.Membresia_Id, membresia.Inscripcion_Membresia_Id, membresia.Lista_Precio_Membresia_Id, membresia.Producto_Id, membresia.Lista_Precio_Producto_Id, membresia.Orden_Venta_Detalle_Descripcion, membresia.Orden_Venta_Detalle_Cantidad, Convert.ToDecimal(membresia.Orden_Venta_Detalle_Importe_Precio), Convert.ToDecimal(membresia.Orden_Venta_Detalle_Importe_Prorrateo), Convert.ToDecimal(membresia.Orden_Venta_Detalle_Importe_Suma), Convert.ToDecimal(membresia.Orden_Venta_Detalle_Importe_Descuento), Convert.ToDecimal(membresia.Orden_Venta_Detalle_Importe_Subtotal), Convert.ToDecimal(membresia.Orden_Venta_Detalle_Importe_Impuesto), Convert.ToDecimal(membresia.Orden_Venta_Detalle_Importe_Total), membresia.Tipos_Servicios);
                         if (ValorDetalle >= 0)
                         {
                             VentaRealizada = true;
@@ -114,7 +106,7 @@ namespace WorklabsMx.iOS
         public override void DidFinishAuthenticationProcess(BeanTokenizeResponse tokenizeResponse, SuiteError error)
         {
             //throw new NotImplementedException();
-            this.GuardarPagoServer(ordenventaLocal, membresiasLocal);
+            this.GuardarPagoServer();
         }
 
         public override void DidFinishPayProcess(string response, SuiteError error)
@@ -125,11 +117,13 @@ namespace WorklabsMx.iOS
 
         public override void DidFinishTokenizeTransantion(BeanPaymentWithToken beanPaymentWithToken, SuiteError error)
         {
+            
             //throw new NotImplementedException();
         }
 
         public override void OperationCanceledByUser()
         {
+            new MessageDialog().SendToast("Operación cancelada por el usuario");
             //throw new NotImplementedException();
         }
     }
