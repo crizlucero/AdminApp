66a7d1e59e40e170d9db7020c61bf71a2faa3311
Christian Lucero <crizlucero@gmail.com>
2018-04-23 18:03:48 -0500

    Push notifications

---
+++ WorklabsMx.Droid/Service/MyFirebaseMessagingService.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Util;
+using Firebase.Messaging;
+
+namespace WorklabsMx.Droid.Service
+{
+    [Service(Label = "FirebaseMessagingService")]
+    [IntentFilter(new String[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+        }
+
+        void SendNotification(string messageBody, IDictionary<string, string> data)
+        {
+            var intent = new Intent(this, typeof(MainActivity));
+            intent.AddFlags(ActivityFlags.ClearTop);
+            foreach (string key in data.Keys)
+            {
+                intent.PutExtra(key, data[key]);
+            }
+            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
+
+            var notificationBuilder = new Notification.Builder(this)
+                .SetSmallIcon(Resource.Mipmap.ic_logo_mini)
+                .SetContentTitle("FCM Message")
+                .SetContentText(messageBody)
+                .SetAutoCancel(true)
+                .SetContentIntent(pendingIntent);
+
+            var notificationManager = NotificationManager.FromContext(this);
+            notificationManager.Notify(0, notificationBuilder.Build());
+        }
+    }
+}
